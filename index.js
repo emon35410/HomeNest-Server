261b619894764da8f17361e1b921b312d8353d9e
emon35410 <mhhsan341@gmail.com>
2025-11-11 09:44:48 +0600

    user data post and get done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-app.use(cors())
+app.use(cors());
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send('HomeNest Is Running!')
@@ -25,44 +25,85 @@ async function run() {
 
     const db = client.db('homenest_db');
     const homeCollection = db.collection('homes')
-// All data
-    app.get("/homes",async(req,res)=>{
-        const cursor = homeCollection.find().sort({price:-1});
+    const userCollection = db.collection('users')
+
+    // Users data
+    app.post("/users", async (req, res) => {
+        const newUser = req.body;
+        const email = newUser.email;
+        const query = { email: email };
+        const existingUser = await userCollection.findOne(query);
+
+        if (existingUser) {
+            return res.json({ success: false, message: "Already Have an Account" });
+        } else {
+            const result = await userCollection.insertOne(newUser);
+            return res.json({ success: true, message: "User saved", data: result });
+        }
+    });
+
+
+    app.get("/users", async (req, res) => {
+        const users = await userCollection.find().toArray();
+        res.send(users);
+    });
+
+    // All data
+    app.get("/homes", async (req, res) => {
+        // find home by email
+        const email = req.query.email;
+        const query = {};
+        if (email) {
+            query.seller_email = email;
+        }
+
+        console.log("Query received:", query);
+
+        const cursor = homeCollection.find(query).sort({ price: -1 });
         const result = await cursor.toArray();
         res.send(result)
     })
+
+
     // recent-homes-data
-    app.get("/recent-homes",async(req,res)=>{
-        const cursor = homeCollection.find().sort({price:-1}).limit(6);
+    app.get("/recent-homes", async (req, res) => {
+        const cursor = homeCollection.find().sort({ price: -1 }).limit(6);
         const result = await cursor.toArray();
         res.send(result)
     })
     // Single Data
-    app.get("/homes/:id",async(req,res)=>{
-        const id = req.params.id;
-        const query ={_id: new ObjectId(id)};
-        const result = await homeCollection.findOne(query);
-        res.send(result)
-    })
+    app.get("/homes/:id", async (req, res) => {
+        try {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await homeCollection.findOne(query);
+            if (!result) return res.status(404).send({ message: "Home not found" });
+            res.send(result);
+        } catch (error) {
+            console.error("Error fetching home:", error);
+            res.status(500).send({ error: "Invalid ID or server error" });
+        }
+    });
 
-    app.post("/homes", async(req,res)=>{
+
+    app.post("/homes", async (req, res) => {
         const newHome = req.body;
         const result = await homeCollection.insertOne(newHome);
         res.send(result);
     })
     // update Homes
-    app.patch("/homes/:id", async(req,res)=>{
+    app.patch("/homes/:id", async (req, res) => {
         const id = req.params.id;
         const updateHome = req.body;
-        const query = {_id: new ObjectId(id)};
-        const update ={
+        const query = { _id: new ObjectId(id) };
+        const update = {
             $set: updateHome
         }
-        const result = await homeCollection.updateOne(query,update);
+        const result = await homeCollection.updateOne(query, update);
         res.send(result)
     })
     // Delete Homes
-    app.delete("/homes/:id", async(req,res)=>{
+    app.delete("/homes/:id", async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const result = await homeCollection.deleteOne(query);
